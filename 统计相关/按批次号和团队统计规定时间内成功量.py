a24f0f5ac86a0470c8b62a6dcf4214b0f6dcf89f
fei <251629543@qq.com>
2019-10-11 17:07:44 +0800

    调整目录结构

--- 统计相关/按批次号和团队统计规定时间内成功量.py
+++ 统计相关/按批次号和团队统计规定时间内成功量.py
@@ -1,5 +1,5 @@
 import pymysql
-import SN_time
+import time
 import datetime
 
 def search_success():
@@ -29,15 +29,15 @@ def search_success():
 
 def S_time():
     sql_time = input("默认查询当前时间段成功量（回车），输入l+回车查询上个时间段数据，或手动输入后回车：")
-    mon, mday, hour = SN_time.localtime().tm_mon, SN_time.localtime().tm_mday, SN_time.localtime().tm_hour
+    mon, mday, hour = time.localtime().tm_mon, time.localtime().tm_mday, time.localtime().tm_hour
     if sql_time == "":
-        if SN_time.localtime().tm_hour >= 12:
+        if time.localtime().tm_hour >= 12:
             sql_time = "'2019-%s-%s 12:00:00' AND '2019-%s-%s 23:00:00'"%(mon, mday, mon, mday)
         else:
             sql_time = "'2019-%s-%s 00:00:00' AND '2019-%s-%s 12:00:00'"%(mon, mday, mon, mday)
 
     if sql_time == "l":
-        if SN_time.localtime().tm_hour >= 12:
+        if time.localtime().tm_hour >= 12:
             sql_time = "'2019-%s-%s 00:00:00' AND '2019-%s-%s 12:00:00'"%(mon, mday, mon, mday)
         else:
             mon = (datetime.datetime.now()+datetime.timedelta(days=-1)).strftime("%m")
